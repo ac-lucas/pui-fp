994abf653b5d07e544319f051a0e1e2475158bf2
ac-lucas <47259349+ac-lucas@users.noreply.github.com>
2022-11-29 22:49:19 -0500

    zoominggg

--- js/app.js
+++ js/app.js
@@ -14,40 +14,19 @@
 
 // svg.call(zoom);
 
-let data = [], width = 600, height = 400, numPoints = 100;
+let data = [], width = 600, height = 400;
 
 let zoom = d3.zoom()
 	.on('zoom', handleZoom);
 
 function handleZoom(e) {
-	d3.select('svg g')
+	d3.selectAll('svg g')
 		.attr('transform', e.transform);
 }
 
 function initZoom() {
-	d3.select('svg')
+	d3.select('div')
 		.call(zoom);
-}
-
-function updateData() {
-	data = [];
-	for(let i=0; i<numPoints; i++) {
-		data.push({
-			id: i,
-			x: Math.random() * width,
-			y: Math.random() * height
-		});
-	}
-}
-
-function update() {
-	d3.select('svg g')
-		.selectAll('circle')
-		.data(data)
-		.join('circle')
-		.attr('cx', function(d) { return d.x; })
-		.attr('cy', function(d) { return d.y; })
-		.attr('r', 3);
 }
 
   Promise.all([
@@ -59,5 +38,3 @@ function update() {
   });
 
 initZoom();
-updateData();
-update();
