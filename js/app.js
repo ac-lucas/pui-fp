2dfce84ce7a70bf5a17a0854599ee5c959e665f6
ac-lucas <47259349+ac-lucas@users.noreply.github.com>
2022-11-30 22:42:41 -0500

    progress on zooming

--- js/app.js
+++ js/app.js
@@ -1,40 +1,33 @@
-// // load and display the people
-// var svg = d3.select("#vis").select(function() {
-//     return this.appendChild(document.getElementById("people"));
 
-// });
 
-// var zoom = d3.zoom()
-//       .scaleExtent([1, 8])
-//       .on('zoom', function(event) {
-//         console.log("hi")
-//           d3.select('svg')
-//            .attr('transform', event.transform);
-// });
-
-// svg.call(zoom);
-
-let data = [], width = 600, height = 400;
+let data = [], width = window.innerHeight, height = window.innerWidth;
 
 let zoom = d3.zoom()
-	.on('zoom', handleZoom);
+  .scaleExtent([.99,1.5])
+  .translateExtent([[0, 0], [0,0]])
+  .extent([[0, 0], [0,0]])
+	.on('zoom', handleZoom)
+
 
 function handleZoom(e) {
 	d3.selectAll('svg g')
-		.attr('transform', e.transform);
+		.attr('transform', e.transform)
 }
 
 function initZoom() {
-	d3.select('div')
-		.call(zoom);
+	d3.selectAll('.vectors')
+		.call(zoom)
 }
 
   Promise.all([
     d3.xml('assets/people.svg'),
-    d3.xml('assets/paris.svg')
+    d3.xml('assets/towns.svg'),
+    d3.xml('assets/paris.svg'),
+    
   ]).then(data => {
     d3.select("#people-container").node().append(data[0].documentElement);
-    d3.select("#paris-container").node().append(data[1].documentElement);
+    d3.select("#town-container").node().append(data[1].documentElement);
+    d3.select("#paris-container").node().append(data[2].documentElement);
   });
 
 initZoom();
